bf3641e957df58747c26d3b84a61ebf3e80aa253
Raihan <raihanhn20@gmail.com>
2023-09-20 21:59:48 +0600

    particles

---
+++ app/face/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Particletsbg from "../components/Particletsbg";
+
+export default function page() {
+  return (
+    <div>
+      <Particletsbg />
+      <h3 className=" text-yellow-500 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ea
+        voluptate ducimus quae quos nostrum aliquid totam nulla maxime
+        perferendis minus magnam atque delectus eos libero sit, provident, eaque
+        fuga vitae aut. Aspernatur aliquid facere odio tempore mollitia vero
+        voluptates porro officiis nisi. Quae tenetur ipsa laudantium libero
+        ducimus voluptate?
+      </h3>
+    </div>
+  );
+}
---
+++ app/face2/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Particlediff from "../components/Particlediff";
+
+export default function page() {
+  return (
+    <div>
+      <Particlediff />
+    </div>
+  );
+}
